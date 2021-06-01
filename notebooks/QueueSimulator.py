9b6ca914e5589c0021a8ba3d8a969f1650dc59c1
GitAYYYer <duc.h@hotmail.com>
2021-06-02 05:05:25 +1000

    basic final scenario 1

--- notebooks/QueueSimulator.py
+++ notebooks/QueueSimulator.py
@@ -13,9 +13,9 @@ class QueueSimulator(gym.Env):
         self.queues = copy.deepcopy(queues_finished_timeslots)
         self.scenario = 1
         
-        # Graph the total wait times for each queue, and at certain episode intervals, add the wait times to the map
-        self.queues_total_wait_times = [[], [], []]
-        self.queues_total_wait_times_map = {}
+        # Graph the total wait times for each packet, and at certain episode intervals, add the wait times to the map
+        self.packets_total_wait_times = [[], [], []]
+        self.packets_total_wait_times_map = {}
         
         # Action Space is 3, because we have 3 queues to choose from
         self.action_space = spaces.Discrete(3)
@@ -24,22 +24,21 @@ class QueueSimulator(gym.Env):
         # so wait times can be 0 or infinity technically.
         self.observation_space = spaces.Box(low=np.array([0, 0, 0]), high=np.array([np.inf, np.inf, np.inf]), dtype=np.dtype(int))
    
-    # Get total wait times for each queue (so how long a queue has been waiting for on a packet to send).
-    # Remember, gets how long EACH packet has been waiting, so can be quite large since its every packet and not just the first.
     def calc_state(self):
         # Use -1 as indicator for packet not arrived. Makes more sense to use something like np.inf, but easier to create q_table with -1 than np.inf
         calc_state = [-1, -1, -1]
         for i, queue in enumerate(self.queues):
-            queue_total_wait = 0
+            # queue = [4, 7, 10, ...]
             packet_arrived = False
-            for packet in queue:
-                if packet <= self.current_timeslot:
-                    queue_total_wait += (self.current_timeslot - packet)
+            packet_wait = -1
+            if queue:    # Check the packet exists first
+                if queue[0] <= self.current_timeslot:
                     packet_arrived = True
+                    packet_wait = self.current_timeslot - queue[0]
                     
             # Only update state wait time if there exists a packet indicated by the packet_arrived boolean. Else the state will stay as -1
             if packet_arrived:
-                calc_state[i] = queue_total_wait
+                calc_state[i] = packet_wait
         return calc_state
 
     def step(self, action):
@@ -49,20 +48,29 @@ class QueueSimulator(gym.Env):
         # Now calc reward
         # If the current_state is -1 then packet has no arrived, and we DO NOT WANT TO reward the model. We shouldnt reward it for no choice
         if current_state[action] == -1:
-            reward = 0
+            reward = -100
         else:
-            # The reward is some arbitrary number divided by the queue's mean_delay, and then multiplied by the packet wait time. 
-            # If the packet has waited its mean delay, then it will cancel out the mean_delay denominator and thus reward will be complete 100.
-            reward = (100 / self.mean_delay_requirements[action]) * (self.current_timeslot - self.queues[action][0])
+            # The reward is some arbitrary number divided by the queue's mean_delay, and then multiplied by the queue wait time. 
+            # If the queue has waited its mean delay, then it will cancel out the mean_delay denominator and thus reward will be complete 100.
+            reward = (100 / self.mean_delay_requirements[action]) * (current_state[action])
+            if reward > 100:
+                reward = 100
             
+        # If best effort queue is chosen, rewrite the reward.
         # Reward when the best effort queue is chosen should be really high, but not as high as when mean_delay_requirement is met (100 reward)
+        # 70 is chosen because for PQ1, if it's waited at least 5/6 of its mean delay requirement, that is (100/6)*5 = 83, which would make it
+        # prioritised over the best effort reward, which is what we want.
+        # Similarly with PQ2, (100/4)*3 = 75, which would also make it more prioritised which is what we want.
         if action == 2:
-            reward = 95
+            if current_state[action] == -1:
+                reward = -100
+            else:
+                reward = 99
         
-        # Everytime you transmit a packet, keep track of how long that packet had to wait in queues_total_wait_times to graph later
+        # Everytime you transmit a packet, keep track of how long that packet had to wait in packets_total_wait_times to graph later
         # also, only delete if that packet actually exists in the queue (i.e. its arrived, which is only at a certain timeslot onwards)
         if (len(self.queues[action]) > 0 and self.queues[action][0] <= self.current_timeslot):
-            self.queues_total_wait_times[action].append(self.current_timeslot - self.queues[action][0])
+            self.packets_total_wait_times[action].append(self.current_timeslot - self.queues[action][0])
             del self.queues[action][0]
         
         # Now get new state to send back
@@ -80,10 +88,10 @@ class QueueSimulator(gym.Env):
     def reset(self, episode):
         interval = 5000//10
         if episode % interval == 0:
-            self.queues_total_wait_times_map.update( {episode: self.queues_total_wait_times} )
+            self.packets_total_wait_times_map.update( {episode: self.packets_total_wait_times} )
         self.current_timeslot = 0
         self.queues = copy.deepcopy(self.queues_finished_timeslots)
-        self.queues_total_wait_times = [[], [], []]
+        self.packets_total_wait_times = [[], [], []]
         return self.calc_state()
         
     def render(self):
@@ -92,10 +100,15 @@ class QueueSimulator(gym.Env):
     # Gets the total wait times from the last episode of the model.
     def calc_average_wait_time(self):
         average_queue_wait = []
-        last_episode = list(self.queues_total_wait_times_map.keys())[-1]
+        last_episode = list(self.packets_total_wait_times_map.keys())[-1]
 
-        for i, queue in enumerate(self.queues_total_wait_times_map.get(last_episode)):
+        for i, queue in enumerate(self.packets_total_wait_times_map.get(last_episode)):
             average_queue_wait.append(round(sum(queue)/len(queue),2))
         
         return average_queue_wait
+
+    # Manually update the map to put the episode in as an entry
+    def update_last_episode_stats(self, episode):
+        self.packets_total_wait_times_map.update( {episode: self.packets_total_wait_times} )
+
  
--- notebooks/QueueSimulatorScenario2.py
+++ notebooks/QueueSimulatorScenario2.py
@@ -15,8 +15,8 @@ class QueueSimulatorScenario2(gym.Env):
         self.scenario = 2
         
         # Graph the total wait times for each queue, and at certain episode intervals, add the wait times to the map
-        self.queues_total_wait_times = [[], [], []]
-        self.queues_total_wait_times_map = {}
+        self.packets_total_wait_times = [[], [], []]
+        self.packets_total_wait_times_map = {}
 
         # Used when we look at state and if we need to add an additional +1 to the timeslots.
         self.previous_queue = -1
@@ -29,22 +29,21 @@ class QueueSimulatorScenario2(gym.Env):
         # so wait times can be 0 or infinity technically.
         self.observation_space = spaces.Box(low=np.array([0, 0, 0]), high=np.array([np.inf, np.inf, np.inf]), dtype=np.dtype(int))
    
-    # Get total wait times for each queue (so how long a queue has been waiting for on a packet to send).
-    # Remember, gets how long EACH packet has been waiting, so can be quite large since its every packet and not just the first.
     def calc_state(self, current_queue):
         # Use -1 as indicator for packet not arrived. Makes more sense to use something like np.inf, but easier to create q_table with -1 than np.inf
         calc_state = [-1, -1, -1, current_queue]
         for i, queue in enumerate(self.queues):
-            queue_total_wait = 0
+            # queue = [4, 7, 10, ...]
             packet_arrived = False
-            for packet in queue:
-                if packet <= self.current_timeslot:
-                    queue_total_wait += (self.current_timeslot - packet)
+            packet_wait = -1
+            if queue:    # Check the packet exists first
+                if queue[0] <= self.current_timeslot:
                     packet_arrived = True
+                    packet_wait = self.current_timeslot - queue[0]
                     
             # Only update state wait time if there exists a packet indicated by the packet_arrived boolean. Else the state will stay as -1
             if packet_arrived:
-                calc_state[i] = queue_total_wait
+                calc_state[i] = packet_wait
         return calc_state
 
     def step(self, action):
@@ -54,20 +53,30 @@ class QueueSimulatorScenario2(gym.Env):
         # Now calc reward
         # If the current_state is -1 then packet has no arrived, and we DO NOT WANT TO reward the model. We shouldnt reward it for no choice
         if current_state[action] == -1:
-            reward = 0
+            reward = -100
         else:
-            # The reward is some arbitrary number divided by the queue's mean_delay, and then multiplied by the packet wait time. 
-            # If the packet has waited its mean delay, then it will cancel out the mean_delay denominator and thus reward will be complete 100.
-            reward = (100 / self.mean_delay_requirements[action]) * (self.current_timeslot - self.queues[action][0])
+            # The reward is some arbitrary number divided by the queue's mean_delay, and then multiplied by the queue wait time. 
+            # If the queue has waited its mean delay, then it will cancel out the mean_delay denominator and thus reward will be complete 100.
+            reward = (100 / self.mean_delay_requirements[action]) * (current_state[action])
             
+        # If best effort queue is chosen, rewrite the reward.
         # Reward when the best effort queue is chosen should be really high, but not as high as when mean_delay_requirement is met (100 reward)
+        # 70 is chosen because for PQ1, if it's waited at least 5/6 of its mean delay requirement, that is (100/6)*5 = 83, which would make it
+        # prioritised over the best effort reward, which is what we want.
+        # Similarly with PQ2, (100/4)*3 = 75, which would also make it more prioritised which is what we want.
         if action == 2:
-            reward = 90
+            if current_state[action] == -1:
+                reward = -100
+            else:
+                reward = 99
+
+        if action == self.previous_queue:
+            reward = reward * 2
         
         # Everytime you transmit a packet, keep track of how long that packet had to wait in queues_total_wait_times to graph later
         # also, only delete if that packet actually exists in the queue (i.e. its arrived, which is only at a certain timeslot onwards)
         if (len(self.queues[action]) > 0 and self.queues[action][0] <= self.current_timeslot):
-            self.queues_total_wait_times[action].append(self.current_timeslot - self.queues[action][0])
+            self.packets_total_wait_times[action].append(self.current_timeslot - self.queues[action][0])
             del self.queues[action][0]
         
         # Now get new state to send back
@@ -89,10 +98,10 @@ class QueueSimulatorScenario2(gym.Env):
     def reset(self, episode):
         interval = 5000//10
         if episode % interval == 0:
-            self.queues_total_wait_times_map.update( {episode: self.queues_total_wait_times} )
+            self.packets_total_wait_times_map.update( {episode: self.packets_total_wait_times} )
         self.current_timeslot = 0
         self.queues = copy.deepcopy(self.queues_finished_timeslots)
-        self.queues_total_wait_times = [[], [], []]
+        self.packets_total_wait_times = [[], [], []]
         self.previous_queue = -1
         self.total_queue_switches = 0
         return self.calc_state(-1)
